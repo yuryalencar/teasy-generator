6969cc557c975d12a867ad34a7b4af283ab67f05
Yury Alencar <yuryalencar19@gmail.com>
2020-08-26 17:30:58 -0300

    refactor(frontend): Start a new frontend project

--- frontend/src/pages/DownloadScriptAndBackup/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const DownloadScriptAndBackup: React.FC = () => {
-    return <h1>Page to make a download of the tests scripts and a tree backup</h1>;
-}
-
-export default DownloadScriptAndBackup;
