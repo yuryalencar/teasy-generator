62d79b3c9c4b1519a2c6b2c08e0464b6e96778d4
Gustavo Bittencourt Satheler <gustavosatheler@gmail.com>
2020-09-22 19:16:59 -0300

    feat(Component): add option selectable as default value

--- frontend/src/components/inputSelect/index.js
+++ frontend/src/components/inputSelect/index.js
@@ -1,25 +1,22 @@
-import React, {useContext, useState} from 'react'
-import { FormGroup,  InputGroup, InputIcone } from './styles'
+import React, { useContext, useState } from 'react'
+import { FormGroup, InputGroup, InputIcone } from './styles'
 import { Hexagon } from 'react-feather'
-import {JsonContext} from "../../context";
+import { JsonContext } from "../../context";
 
 const InputSelect = () => {
-  const {pages, onChangePage } = useContext(JsonContext)
-  
+  const { pages, onChangePage } = useContext(JsonContext)
+
   return (
     <FormGroup>
       <InputGroup>
-        <InputIcone>{<Hexagon/>}</InputIcone>
+        <InputIcone>{<Hexagon />}</InputIcone>
         <select className="form-control" onChange={(e) => onChangePage(pages[e.target.value])}>
-          <option value={null} disabled selected hidden> </option>
+          <option value={null} selected>Selecione uma opção</option>
           {pages.map((page, i) => {
-            return(
-                <option key={i} value={i}>{page.name}</option>
-            )
+            return (<option key={i} value={i}>{page.name}</option>)
           })}
-          
         </select>
-    
+
       </InputGroup>
     </FormGroup>
   )
